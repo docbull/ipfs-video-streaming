c180863eca4c4e2e657611d61e514826795b2d4a
docbull <fudghks21@gmail.com>
2022-04-03 15:49:47 +0900

    ExpressJS router file

--- routes/router.js
+++ routes/router.js
@@ -2,4 +2,7 @@ module.exports = function (app) {
     app.get('/', function(req, res) {
         res.render('index.ejs')
     })
+    app.get('/player', function(req, res) {
+        res.render('player.ejs')
+    })
 }
