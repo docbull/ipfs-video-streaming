98a1d16f4e9bf4190882b9409994a70d396308f8
docbull <fudghks21@gmail.com>
2022-04-03 21:55:01 +0900

    HTTPS-based service

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-/*
-import express from 'express';
-import path from 'path';
-import cors from 'cors';
-*/
-
-const Bundler = require('parcel-bundler');
+//const Bundler = require('parcel-bundler');
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 
-const app = express();
+const HTTP_PORT = 8888;
+const HTTPS_PORT = 11800;
+
 const PORT = 11800;
 
-const options = {};
+const options = {
+    key: fs.readFileSync('./ssl/rootca.key'),
+    cert: fs.readFileSync('./ssl/rootca.crt')
+};
+
+const app = express();
 
 const file = './views/index.ejs';
 
-const bundler = new Bundler(file, options);
+//const bundler = new Bundler(file, options);
 
 const router = require('./routes/router')(app);
 
@@ -23,48 +27,13 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.engine('html', require('ejs').renderFile);
 
-// img folder includes icon image file
 app.use(express.static(path.join(__dirname, '/img')));
-// hls-streaming
 app.use(express.static(path.join(__dirname, '/public')));
-
 app.use(express.static(path.join(__dirname, '/js')));
 
-app.use(bundler.middleware());
+//app.use(bundler.middleware());
 
-function handleListen() {
-    console.log(`Server running at ${PORT} port`);
-}
+app.get('/', (req, res) => { res.json({ message: `Server is running on port ${req.secure ? HTTPS_PORT : HTTP_PORT}` }); }); 
 
-app.listen(PORT, handleListen);
-
-/*
-const express = require('express');
-const path = require('path');
-
-const app = express();
-const PORT = 11800;
-
-const router = require('./routes/router')(app);
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
-app.engine('html', require('ejs').renderFile);
-
-//app.use(cors());
-
-// img folder includes icon image file
-app.use(express.static(path.join(__dirname, '/img')));
-// hls-streaming
-app.use(express.static(path.join(__dirname, '/public')));
-
-app.use(express.static(path.join(__dirname, '/js')));
-
-app.use(express.static(path.join(__dirname, '/hls-streaming/hls-video-streaming')));
-
-function handleListen() {
-    console.log(`Server running at ${PORT} port`);
-}
-
-app.listen(PORT, handleListen);
-*/
+http.createServer(app).listen(HTTP_PORT);
+https.createServer(options, app).listen(HTTPS_PORT);
