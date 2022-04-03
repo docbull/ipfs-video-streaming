96756b391c3f61448c38261566046ee0168518d4
docbull <fudghks21@gmail.com>
2022-04-03 15:46:55 +0900

    Web 3.0 streaming test files

--- app.js
+++ app.js
@@ -1,22 +1,70 @@
-const express = require('express')
-const app = express()
-const PORT = 11800
+/*
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+*/
 
-const router = require('./router/main')(app);
+const Bundler = require('parcel-bundler');
+const express = require('express');
+const path = require('path');
 
-app.set('views', __dirname + '/views');
+const app = express();
+const PORT = 11800;
+
+const options = {};
+
+const file = './views/index.ejs';
+
+const bundler = new Bundler(file, options);
+
+const router = require('./routes/router')(app);
+
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 app.engine('html', require('ejs').renderFile);
 
-app.use(express.static(__dirname + '/img'))
-app.use(express.static(__dirname + '/hls-player/hls-video-streaming'))
+// img folder includes icon image file
+app.use(express.static(path.join(__dirname, '/img')));
+// hls-streaming
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(express.static(path.join(__dirname, '/js')));
+
+app.use(bundler.middleware());
 
 function handleListen() {
-    console.log(`Server listening port: ${PORT}`)
+    console.log(`Server running at ${PORT} port`);
 }
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+app.listen(PORT, handleListen);
+
+/*
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 11800;
+
+const router = require('./routes/router')(app);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+app.engine('html', require('ejs').renderFile);
+
+//app.use(cors());
+
+// img folder includes icon image file
+app.use(express.static(path.join(__dirname, '/img')));
+// hls-streaming
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(express.static(path.join(__dirname, '/js')));
+
+app.use(express.static(path.join(__dirname, '/hls-streaming/hls-video-streaming')));
+
+function handleListen() {
+    console.log(`Server running at ${PORT} port`);
+}
 
 app.listen(PORT, handleListen);
+*/
