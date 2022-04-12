dfe85ba98853d2cb57b6685afd8c789d6547267f
docbull <fudghks21@gmail.com>
2022-04-13 00:57:51 +0900

    IPFS swarm logs are updated

--- js/streaming.js
+++ js/streaming.js
@@ -5,35 +5,36 @@ async function initIPFS(CID) {
   const node = await Ipfs.create({
     repo: repoPath,
     config: {
-      /*
       Bootstrap: [
+        /*
+        '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
+        '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+        '/dnsaddr/bootstrap.libp2p.io/p2p/QmZa1sAxajnQjVM8WjWXoMbmPd7NsWhfKsPkErzpm9wGkp',
+        '/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa',
+        '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt',
+        */
+        '/dns4/node0.preload.ipfs.io/tcp/443/wss/p2p/QmZMxNdpMkewiVZLMRxaNxUeZpDUb34pWjZ1kZvsd16Zic',
+        /*
+        '/dns4/node1.preload.ipfs.io/tcp/443/wss/p2p/Qmbut9Ywz9YEDrz8ySBSgWyJk41Uvm2QJPhwDJzJyGFsD6',
+        '/dns4/node2.preload.ipfs.io/tcp/443/wss/p2p/QmV7gnbW5VTcJ3oyM2Xk1rdFBJ3kTkvxc87UFGsun29STS',
+        '/dns4/node3.preload.ipfs.io/tcp/443/wss/p2p/QmY7JB6MQXhxHvq7dBDh4HpbH29v4yE9JRadAVpndvzySN',
+        */
         '/ip4/203.247.240.228/tcp/4001/p2p/12D3KooWKLPAVDieCLqV4FhjRXpUZ9biPsKbWWLduBojr5doifh6',
-        '/ip4/203.247.240.228/tcp/4002/p2p/12D3KooWDpp2acwvwvHq3ASUQFNY13U5mNC2RhAqw8LC5iUjyqGD',
-        '/ip4/203.247.240.228/tcp/4003/ws/p2p/12D3KooWDpp2acwvwvHq3ASUQFNY13U5mNC2RhAqw8LC5iUjyqGD',
-        '/ip4/147.75.109.213/tcp/4001/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
-        '/ip4/147.75.83.83/tcp/4001/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
-        '/ip4/127.0.0.1/tcp/4001/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
-        '/ip6/2604:1380:2000: 7a00::1/tcp/4001/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
-        '/ip6/::1/tcp/4001/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
-        '/ip4/147.75.83.83/udp/4001/quic/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
-        '/ip4/127.0.0.1/udp/4001/quic/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
-        '/ip6/2604:1380:2000:7a00::1/udp/4001/quic/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
-        '/ip6/::1/udp/4001/quic/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
-        '/ip4/127.0.0.1/tcp/8081/ws/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
-        '/ip6/64:ff9b::934b:5353/udp/4001/quic/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb'
+        '/ip4/127.0.0.1/tcp/4001/p2p/12D3KooWKLPAVDieCLqV4FhjRXpUZ9biPsKbWWLduBojr5doifh6',
+        '/ip4/203.247.240.228/tcp/4001/p2p/12D3KooWKLPAVDieCLqV4FhjRXpUZ9biPsKbWWLduBojr5doifh6',
+        '/ip4/203.247.240.228/udp/4001/quic/p2p/12D3KooWKLPAVDieCLqV4FhjRXpUZ9biPsKbWWLduBojr5doifh6',
+        '/ip6/::1/tcp/4001/p2p/12D3KooWKLPAVDieCLqV4FhjRXpUZ9biPsKbWWLduBojr5doifh6',
+        '/ip6/::1/udp/4001/quic/p2p/12D3KooWKLPAVDieCLqV4FhjRXpUZ9biPsKbWWLduBojr5doifh6'
       ]
-      */
     }
   });
   let end = new Date();
   console.log(`IPFS create: ${end-start}ms`);
 
   const peerID = '/ip4/203.247.240.228/tcp/4001/p2p/12D3KooWKLPAVDieCLqV4FhjRXpUZ9biPsKbWWLduBojr5doifh6';
-  //const peerID = '/ip4/203.247.240.228/tcp/9090/ws/p2p-webrtc-star';
-  
-  addBootstrapPeer(node, peerID);
-  //printBootstrapPeers(node);
-  setTimeout(() => printSwarmAddrs(node), 5000);
+
+  printSwarmAddrs(node);
+  setTimeout(() => printSwarmPeers(node), 3000);
 
   Hls.DefaultConfig.loader = HlsjsIpfsLoader;
   Hls.DefaultConfig.debug = false;
@@ -75,7 +76,7 @@ async function addBootstrapPeer(node, peerID) {
 
 async function printSwarmPeers(node) {
   const conn = await node.swarm.peers();
-  console.log('-------- Swarm Peers --------');
+  console.log('--- Connected Swarm Peers ---');
   conn.forEach(peer => {
     console.log(peer);
   });
@@ -84,7 +85,7 @@ async function printSwarmPeers(node) {
 
 async function printSwarmAddrs(node) {
   const peerInfos = await node.swarm.addrs();
-  console.log('--- Connected Swarm Addrs ---');
+  console.log('-------- Swarm Addrs --------');
   peerInfos.forEach(info => {
     console.log(info.id);
     info.addrs.forEach(addr => console.log(addr.toString()));
