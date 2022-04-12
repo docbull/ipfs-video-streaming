aeb17ef19c0245971143039f71eb5a642a02e2b3
docbull <fudghks21@gmail.com>
2022-04-13 00:25:32 +0900

    IPFS nodes' logs are updated

--- js/streaming.js
+++ js/streaming.js
@@ -4,20 +4,36 @@ async function initIPFS(CID) {
   let start = new Date();
   const node = await Ipfs.create({
     repo: repoPath,
-    /*
     config: {
+      /*
       Bootstrap: [
-        '/ip4/203.247.240.228/tcp/4001/p2p/12D3KooWKLPAVDieCLqV4FhjRXpUZ9biPsKbWWLduBojr5doifh6'
+        '/ip4/203.247.240.228/tcp/4001/p2p/12D3KooWKLPAVDieCLqV4FhjRXpUZ9biPsKbWWLduBojr5doifh6',
+        '/ip4/203.247.240.228/tcp/4002/p2p/12D3KooWDpp2acwvwvHq3ASUQFNY13U5mNC2RhAqw8LC5iUjyqGD',
+        '/ip4/203.247.240.228/tcp/4003/ws/p2p/12D3KooWDpp2acwvwvHq3ASUQFNY13U5mNC2RhAqw8LC5iUjyqGD',
+        '/ip4/147.75.109.213/tcp/4001/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
+        '/ip4/147.75.83.83/tcp/4001/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+        '/ip4/127.0.0.1/tcp/4001/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+        '/ip6/2604:1380:2000: 7a00::1/tcp/4001/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+        '/ip6/::1/tcp/4001/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+        '/ip4/147.75.83.83/udp/4001/quic/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+        '/ip4/127.0.0.1/udp/4001/quic/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+        '/ip6/2604:1380:2000:7a00::1/udp/4001/quic/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+        '/ip6/::1/udp/4001/quic/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+        '/ip4/127.0.0.1/tcp/8081/ws/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+        '/ip6/64:ff9b::934b:5353/udp/4001/quic/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb'
       ]
+      */
     }
-    */
   });
   let end = new Date();
   console.log(`IPFS create: ${end-start}ms`);
 
-  let peerID = '/ip4/203.247.240.228/tcp/4001/p2p/12D3KooWKLPAVDieCLqV4FhjRXpUZ9biPsKbWWLduBojr5doifh6';
-
+  const peerID = '/ip4/203.247.240.228/tcp/4001/p2p/12D3KooWKLPAVDieCLqV4FhjRXpUZ9biPsKbWWLduBojr5doifh6';
+  //const peerID = '/ip4/203.247.240.228/tcp/9090/ws/p2p-webrtc-star';
+  
   addBootstrapPeer(node, peerID);
+  //printBootstrapPeers(node);
+  setTimeout(() => printSwarmAddrs(node), 5000);
 
   Hls.DefaultConfig.loader = HlsjsIpfsLoader;
   Hls.DefaultConfig.debug = false;
@@ -44,23 +60,43 @@ async function initIPFS(CID) {
 // printBootstrapPeers prints all bootstrapping peers.
 async function printBootstrapPeers(node) {
   const peerList = await node.bootstrap.list();
+  console.log('------ Bootstrap Peers ------');
   for(let peer of peerList.Peers) {
     console.log(peer.toString());
   }
+  console.log('-----------------------------');
 }
 
 // addBootstrapPeer adds bootstrapping peer into the bootstrap peer list.
 async function addBootstrapPeer(node, peerID) {
   let res = await node.bootstrap.add(peerID);
-  console.log(res);
+  console.log('added peer:', res.Peers.toString());
 }
 
 async function printSwarmPeers(node) {
-  let conn = await node.swarm.peers();
-  console.log(conn);
+  const conn = await node.swarm.peers();
+  console.log('-------- Swarm Peers --------');
+  conn.forEach(peer => {
+    console.log(peer);
+  });
+  console.log('-----------------------------');
+}
+
+async function printSwarmAddrs(node) {
+  const peerInfos = await node.swarm.addrs();
+  console.log('--- Connected Swarm Addrs ---');
+  peerInfos.forEach(info => {
+    console.log(info.id);
+    info.addrs.forEach(addr => console.log(addr.toString()));
+  })
+  console.log('-----------------------------');
 }
 
 async function addSwarmPeer(node, peerID) {
   await node.swarm.connect(peerID);
-  printSwarmPeers(node);
+}
+
+async function localAddress(node) {
+  const multiAddrs = await node.swarm.localAddrs();
+  console.log(multiAddrs);
 }
