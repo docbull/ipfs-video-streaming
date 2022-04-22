19c7546ad2b764e521dcfcfb3a8a68115c2f3c11
docbull <fudghks21@gmail.com>
2022-04-22 17:34:41 +0900

    NFT sample files from https://docs.alchemy.com/alchemy/tutorials/how-to-create-an-nft

---
+++ nft/my-nft/scripts/deploy.js
@@ -0,0 +1,17 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+    // Grab the contract factory
+    const MyNFT = await ethers.getContractFactory("MyNFT");
+
+    // Start deployment, returning a promise that resolves to a contract object
+    const myNFT = await MyNFT.deploy();
+    console.log("Contract deployed to address:", myNFT.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
