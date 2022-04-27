9bd9cef6d569444bee2801a254fabe7b50bebdbc
docbull <fudghks21@gmail.com>
2022-04-27 16:16:22 +0900

    moved to localhost-hls-streaming

--- hls-streaming/hls-video-streaming/node_modules/test-util-ipfs-example/index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const playwright = require('./playwright/index')
-const node = require('./node/index')
-
-module.exports = {
-  playwright,
-  node
-}
--- hls-streaming/hls-video-streaming/node_modules/test-util-ipfs-example/playwright/index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const servers = require('./servers')
-const daemons = require('./daemons')
-
-module.exports = {
-  servers,
-  daemons
-}
