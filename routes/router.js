9236a5f9a353d6f7a32eb3db1cdce1d923e40b02
docbull <fudghks21@gmail.com>
2022-05-03 01:05:41 +0900

    Express router file

--- routes/router.js
+++ routes/router.js
@@ -1,4 +1,4 @@
-import { encodeHLS } from '../public/uploader/hls-encoder.js';
+import { uploader } from '../public/uploading/uploader.js';
 
 module.exports = function (app) {
     app.get('/', function(req, res) {
@@ -8,26 +8,26 @@ module.exports = function (app) {
         res.render('player.ejs')
     })
 
-    app.post('/upload', (req, res) => {
+    // when you upload a video using the top-right upload button, and it posts the video
+    // on the media server and the server encodes the video using HLS protocol.
+    app.post('/upload', async (req, res) => {
         if (req.files) {
             console.log(req.files);
             var file = req.files.uploading;
             var filename = file.name;
             let title = filename.split('.');
-            console.log(title[0]);
-    
             file.mv('./data/' + filename, function (err) {
                 if (err) {
                     res.render('index.ejs');
                     console.log(err);
                 } else {
+                    // fileInfo indicates information of uploaded video. It contains filename 
+                    // (e.g., example.mp4) and title (e.g., example).
                     const fileInfo = ({
                         video: filename,
                         title: title[0]
                     })
-                    encodeHLS(fileInfo);
-
-                    res.render('index.ejs');
+                    uploader(fileInfo).then(result => res.render('index.ejs'));
                 }
             });
         }
